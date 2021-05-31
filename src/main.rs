e9065b7e7f683bf788874bbcb993c589ce5f43a6
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-05-31 12:17:16 +0000

    press q to quit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+        println!("{}", c);
+        if c == 'q' {
+            break;
+        }
+    }
 }
