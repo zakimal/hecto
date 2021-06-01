2fd20970785d6e9fecef3c641bc69bae1868abf9
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-06-01 04:01:32 +0000

    clear lines one at a time

--- src/editor.rs
+++ src/editor.rs
@@ -32,9 +32,9 @@ impl Editor {
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::cursor_hide();
-        Terminal::clear_screen();
         Terminal::cursor_position(0, 0);
         if self.should_quit {
+            Terminal::clear_screen();
             println!("Goodbye.\r");
         } else {
             self.draw_rows();
@@ -55,6 +55,7 @@ impl Editor {
 
     fn draw_rows(&self) {
         for _ in 0..self.terminal.size().height - 1 {
+            Terminal::clear_current_line();
             println!("~\r");
         }
     }
--- src/terminal.rs
+++ src/terminal.rs
@@ -58,4 +58,8 @@ impl Terminal {
     pub fn cursor_show() {
         print!("{}", termion::cursor::Show);
     }
+
+    pub fn clear_current_line() {
+        print!("{}", termion::clear::CurrentLine);
+    }
 }
