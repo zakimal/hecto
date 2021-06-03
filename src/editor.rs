a0be3165f74c62c308d2e7ea9ad17756d7c6f5fa
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-06-03 00:15:58 +0000

    insert ordinary characters

--- src/editor.rs
+++ src/editor.rs
@@ -106,6 +106,10 @@ impl Editor {
         let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
+            Key::Char(c) => {
+                self.document.insert(&self.cursor_position, c);
+                self.move_cursor(Key::Right);
+            }
             Key::Up
             | Key::Down
             | Key::Left
@@ -141,7 +145,6 @@ impl Editor {
     fn move_cursor(&mut self, key: Key) {
         let terminal_height = self.terminal.size().height as usize;
         let Position { mut y, mut x } = self.cursor_position;
-        let size = self.terminal.size();
         let height = self.document.len();
         let mut width = if let Some(row) = self.document.row(y) {
             row.len()
