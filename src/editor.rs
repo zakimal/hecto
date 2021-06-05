4ac1ef2a8b44a225267ea6917a747085a103a12c
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-06-05 04:22:52 +0000

    colorful search result

--- src/editor.rs
+++ src/editor.rs
@@ -156,6 +156,7 @@ impl Editor {
                     } else if moved {
                         editor.move_cursor(Key::Left);
                     }
+                    editor.document.highlight(Some(query));
                 },
             )
             .unwrap_or(None);
@@ -164,6 +165,7 @@ impl Editor {
             self.cursor_position = old_position;
             self.scroll();
         }
+        self.document.highlight(None);
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
